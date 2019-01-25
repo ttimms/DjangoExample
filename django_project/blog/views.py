faee250269226f0bc7ba2e3618e42c0892ac7588
Tyler <ttimms@kent.edu>
2019-01-24 21:14:56 -0500

    end of video 3. templates and bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Tyler',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 24, 2019'
+    },
+    {
+        'author': 'Dillon',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 27, 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'about'})
+
