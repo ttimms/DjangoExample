a5a5ea0e1633203323cdcbe8a1a9d5cd63f431a7
Tyler <ttimms@kent.edu>
2019-01-25 11:47:24 -0500

    end of video 5. database/migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Tyler',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 24, 2019'
-    },
-    {
-        'author': 'Dillon',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 27, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
